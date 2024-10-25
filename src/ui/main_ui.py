129857e61a5a857f23201d88bf8f1027fc0c9a1e
Edsondacosta000 <duartefragadacosta@gmail.com>
2024-10-26 00:02:46 +0100

    Main UI work

--- src/ui/main_ui.py
+++ src/ui/main_ui.py
@@ -54,8 +54,6 @@ class TranscriptionApp(QWidget):
         menu_layout.setSpacing(8)
         
         self.upload_button = QPushButton('Upload and Transcribe Audio')
-        self.settings_button = QPushButton('Settings')
-        self.help_button = QPushButton('Help')
         
         # Use native button styling
         button_style = """
@@ -67,12 +65,9 @@ class TranscriptionApp(QWidget):
             }
         """
         
-        for button in [self.upload_button, self.settings_button, self.help_button]:
-            button.setStyleSheet(button_style)
+        self.upload_button.setStyleSheet(button_style)
         
         menu_layout.addWidget(self.upload_button)
-        menu_layout.addWidget(self.settings_button)
-        menu_layout.addWidget(self.help_button)
         menu_layout.addStretch()
         
         main_layout.addLayout(menu_layout)
@@ -108,8 +103,6 @@ class TranscriptionApp(QWidget):
         
         # Connect buttons
         self.upload_button.clicked.connect(self.transcription_widget.upload_and_transcribe)
-        self.settings_button.clicked.connect(self.open_settings)
-        self.help_button.clicked.connect(self.show_help)
         
     def update_theme(self, theme):
         colors = self.theme_manager.get_colors(theme)
@@ -156,15 +149,6 @@ class TranscriptionApp(QWidget):
         self.chatgpt.response_received.connect(self.chat_widget.add_response)
         self.chatgpt.error_occurred.connect(self.chat_widget.add_response)
         self.chat_widget.new_question.connect(self.ask_chatgpt)
-
-    def open_settings(self):
-        dialog = SettingsDialog(self)
-        if dialog.exec_():
-            self.chatgpt.load_settings()
-
-    def show_help(self):
-        dialog = HelpDialog(self)
-        dialog.exec_()
             
     def ask_chatgpt(self, question):
         transcript = self.transcription_widget.get_transcript()
